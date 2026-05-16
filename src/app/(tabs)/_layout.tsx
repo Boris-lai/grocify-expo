07e8217cd689b281bed43452a9d7dab5b0121575
Boris <borislai0713@gmail.com>
2026-05-16 14:56:32 +0800

    feat: implement grocery list components with pending and completed items display

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { useGroceryStore } from "@/store/grocery-store";
 import { useAuth } from "@clerk/expo";
 import { Redirect } from "expo-router";
-import { NativeTabs } from "expo-router/build/native-tabs";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 import { useColorScheme } from "nativewind";
 import { useEffect } from "react";
 
@@ -41,10 +41,7 @@ export default function TabsLayout() {
 
       <NativeTabs.Trigger name="planner">
         <NativeTabs.Trigger.Icon
-          sf={{
-            default: "plus.circle",
-            selected: "plus.circle.fill",
-          }}
+          sf={{ default: "plus.circle", selected: "plus.circle.fill" }}
           md="add"
         />
         <NativeTabs.Trigger.Label>Planner</NativeTabs.Trigger.Label>
@@ -52,10 +49,7 @@ export default function TabsLayout() {
 
       <NativeTabs.Trigger name="insights">
         <NativeTabs.Trigger.Icon
-          sf={{
-            default: "chart.bar",
-            selected: "chart.bar.fill",
-          }}
+          sf={{ default: "chart.bar", selected: "chart.bar.fill" }}
           md="analytics"
         />
         <NativeTabs.Trigger.Label>Insights</NativeTabs.Trigger.Label>
